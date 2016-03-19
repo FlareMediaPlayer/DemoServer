664187f063fdb4ca8c1c7fe3498107587c0eac47
Jose  Ortiz <joseortizcosta@icloud.com>
2016-03-18 21:40:06 -0700

    Added Packages created by brian

---
+++ src/Video/VideoParser.java
@@ -0,0 +1,13 @@
+package Video;
+
+/**
+ *
+ * @author csc 668/868 Team #2
+ * This class is just a prototype to start working. Some default parameters 
+ * added here such as socket may be replaced later by any other functionality.
+ */
+
+public class VideoParser 
+{
+    
+}
