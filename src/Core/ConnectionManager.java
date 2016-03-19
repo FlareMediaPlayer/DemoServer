0d813a4320d7a4ec2959bfe1d8c413f7bc775c06
Jose  Ortiz <joseortizcosta@icloud.com>
2016-03-18 21:29:20 -0700

    Initialized Repository

---
+++ src/Core/ConnectionManager.java
@@ -0,0 +1,15 @@
+
+package Core;
+
+/**
+ *
+ * @author csc 668/868 Team #2
+ * This class is just a prototype to start working. Some default parameters 
+ * added here may be replaced later by any other functionality.
+ * 
+ */
+public class ConnectionManager 
+{
+    
+    
+}
