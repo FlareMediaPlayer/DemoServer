dd71bfed1860d42b2220b7e8928e668108872f4c
brianxautumn <shred@brianautumn.com>
2016-03-20 13:13:33 -0700

    Implemented a very basic websocket handshake. Now connects

--- src/Core/FlareMediaPlayerServer.java
+++ src/Core/FlareMediaPlayerServer.java
@@ -1,8 +1,15 @@
 package Core;
-
+import org.apache.commons.codec.binary.Base64;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  *
@@ -12,6 +19,10 @@ public class FlareMediaPlayerServer {
 
     private ServerSocket serverSocket;
     private boolean running = true;
+    
+    private String webSocketKey;
+    private String GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+    private String socketResponseKey;
 
     // Singleton Instance
     private static FlareMediaPlayerServer mediaServer;
@@ -29,6 +40,55 @@ public class FlareMediaPlayerServer {
                     Socket clientSocket = serverSocket.accept();
                     System.out.println(clientSocket.getInetAddress().getHostAddress() + " is connecting");
 
+                    /**
+                     *
+                     * Just testing some data
+                     */
+                    InputStream stream = clientSocket.getInputStream();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(stream));
+
+                    try {
+                        // read the first line to get the request method, URI and HTTP version
+                        String line = in.readLine();
+
+                        while (line != null && line.trim().length() > 0) {
+                            
+                                StringTokenizer st = new StringTokenizer(line);
+                                String label = st.nextToken();
+
+                                if(label.equals("Sec-WebSocket-Key:")){
+                                    webSocketKey = st.nextToken().trim();
+                                }
+                               
+                
+                            line = in.readLine();
+                        }
+
+                        socketResponseKey = Base64.encodeBase64String(DigestUtils.sha1(webSocketKey + GUID));
+
+                        
+
+                    } catch (IOException e) {
+                        System.out.println("Error reading body");
+
+                    }
+                    
+                    //Now We Respond!
+                    BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
+                    PrintWriter writer = new PrintWriter(out, true);  // char output to the client
+                    
+                    writer.println("HTTP/1.1 101 Switching Protocols");
+                    writer.println("Upgrade: websocket");
+                    writer.println("Connection: Upgrade");
+                    writer.println("Sec-WebSocket-Accept: " + socketResponseKey);
+
+                    writer.println("");
+
+                    /**
+                     * End Testing
+                     */
+                    
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
@@ -39,6 +99,7 @@ public class FlareMediaPlayerServer {
     }
 
     public static void main(String[] args) {
+    
         try {
             System.out.println("Initializing Server");
 
