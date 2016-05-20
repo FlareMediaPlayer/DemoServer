ed7b006e85b664b24a5005bcbb5aeb5cd67a3833
LIBEXTMAC <LIBEXTMAC@LIBEXT-MAC-3.local>
2016-05-19 21:28:42 -0700

    Added javadoc

--- src/FlareMessage/FrameMessage.java
+++ src/FlareMessage/FrameMessage.java
@@ -5,6 +5,7 @@
  */
 package FlareMessage;
 
+import static FlareMessage.FlareMessage.HEADER_LENGTH;
 import FlareProtocol.FlareOpCode;
 import Utility.ByteUtils;
 import java.awt.image.BufferedImage;
@@ -17,35 +18,52 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 /**
+ * This class adds the header to the frame data and adds the entire message to a
+ * byte array.
  *
- * @author mac
+ * @author Sapan
  */
-public class FrameMessage  extends FlareMessage{
-    
+public class FrameMessage extends FlareMessage {
+
     //private Frame frame;
     private BufferedImage frame;
-    protected ByteArrayOutputStream byteStream = new ByteArrayOutputStream(); 
+    protected ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
     private int index;
 
-    
-    public FrameMessage(){
+    /**
+     * Sets the appropriate OpCode for FrameMessage.
+     */
+    public FrameMessage() {
         flareOpCode = FlareOpCode.FRAME;
         //System.out.println("frame code is " + flareOpCode);
     }
-    
-    // sets frame to bufferedImage
-    public void setFrame(BufferedImage frame){
-        
-        this.frame = frame;
-        
-    }
-    
 
-    
-    public void setIndex(int index){
+    /**
+     * Sets frame to bufferedImage.
+     *
+     * @param frame
+     */
+    public void setFrame(BufferedImage frame) {
+
+        this.frame = frame;
+
+    }
+
+    /**
+     * Sets index.
+     *
+     * @param index
+     */
+    public void setIndex(int index) {
         this.index = index;
     }
-    
+
+    /**
+     * It adds the header (messageLength and OpCode) to the video data, sends
+     * the entire message to a byte array and returns the byte array.
+     *
+     * @return
+     */
     @Override
     public byte[] toBinary() {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -57,30 +75,22 @@ public class FrameMessage  extends FlareMessage{
         byte[] frameBytes = baos.toByteArray();
 
         dataLength = 4 + frameBytes.length; //4 bytes for index + total binary length
-        messageLength =  dataLength + HEADER_LENGTH;
-         
-           
-        
+        messageLength = dataLength + HEADER_LENGTH;
+
         try {
-            byteStream.reset(); 
+            byteStream.reset();
             byteStream.write(ByteUtils.intToByteArray(messageLength)); // total length   
             byteStream.write(flareOpCode);//op code
             byteStream.write(ByteUtils.intToByteArray(index));//frame number
             byteStream.write(frameBytes);
             byteStream.flush();
-            
-            
-   
-            
-         
+
         } catch (IOException ex) {
             Logger.getLogger(FrameMessage.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println(ex.getMessage());
         }
 
-        
-        
-       return byteStream.toByteArray();
+        return byteStream.toByteArray();
     }
-    
+
 }
--- src/FlareMessage/OpenVideoMessage.java
+++ src/FlareMessage/OpenVideoMessage.java
@@ -5,14 +5,17 @@
  */
 package FlareMessage;
 
+import static FlareMessage.FlareMessage.HEADER_LENGTH;
 import FlareProtocol.FlareOpCode;
 
 /**
+ * This class adds the header to the dataLength. The header contains the
+ * messageLength and OpCode. It then adds the entire message to a byte array.
  *
- * @author mac
+ * @author Sapan
  */
-public class OpenVideoMessage extends FlareMessage{
-    
+public class OpenVideoMessage extends FlareMessage {
+
     boolean videoIsAvailable;
     int height;
     int width;
@@ -20,99 +23,127 @@ public class OpenVideoMessage extends FlareMessage{
     double duration;
     int frameCount;
 
-    
+    /**
+     * This method sets the video availability to default i.e. false and sets
+     * the appropriate OpCode.
+     */
     public OpenVideoMessage() {
-        
+
         videoIsAvailable = false;
         flareOpCode = FlareOpCode.OPEN_VIDEO;
-        
+
     }
-    
-    public void setVideoAvailability(boolean available){
-        
+
+    /**
+     * Sets actual video availability.
+     *
+     * @param available
+     */
+    public void setVideoAvailability(boolean available) {
+
         videoIsAvailable = available;
-        
+
     }
-    
-    public void setHeight(int height){
+
+    /**
+     * Sets video height.
+     *
+     * @param height
+     */
+    public void setHeight(int height) {
         this.height = height;
     }
-    
-    public void setWidth(int width){
+
+    /**
+     * Sets video width.
+     *
+     * @param width
+     */
+    public void setWidth(int width) {
         this.width = width;
     }
-    
-    public void setFps(double fps){
+
+    /**
+     * Sets video frames per second.
+     *
+     * @param fps
+     */
+    public void setFps(double fps) {
         this.fps = fps;
     }
-    
-    public void setDuration(double duration){
+
+    /**
+     * Sets video duration.
+     *
+     * @param duration
+     */
+    public void setDuration(double duration) {
         this.duration = duration;
     }
-    
-    public void setFrameCount(int frameCount){
+
+    /**
+     * Sets the number of frames in the video.
+     *
+     * @param frameCount
+     */
+    public void setFrameCount(int frameCount) {
         this.frameCount = frameCount;
     }
 
+    /**
+     * Checks if the video is available. If yes, it adds the header
+     * (messageLength and OpCode) to the video data and sends the entire message
+     * to a byte array and returns the byte array.
+     *
+     * @return
+     */
     @Override
     public byte[] toBinary() {
-       byte[] data = null;
-       
-       
-       if(!videoIsAvailable){
-           
-           
-           
-           dataLength = 1;
-           messageLength =  dataLength + HEADER_LENGTH;
-           data = new byte[messageLength];
-           
-           //Put Message Length
-           FlareMessage.intToData(data, 0, messageLength);
-           data[4] = flareOpCode;
-           
-           
-           
-           
-           //Followed by (byte) 0
-           data[5] = 0;
-           //System.out.println(data[5]);
-           
-           
-           
-       }else{
-           
-           dataLength = 29;
-           messageLength =  dataLength + HEADER_LENGTH;
-           data = new byte[messageLength];
-           
-           
-           FlareMessage.intToData(data, 0, messageLength);
-           data[4] = flareOpCode;
-           
-           //Followed by (byte) 0
-           data[5] = 1;
-           
-           //width
-           FlareMessage.intToData(data, 6, width);
-           
-           //height
-           FlareMessage.intToData(data, 10, height);
-           
-           //fps
-           FlareMessage.doubleToData(data, 14, fps);
-           
-           //duration
-           FlareMessage.doubleToData(data, 22, duration);
-           
-           
-           FlareMessage.intToData(data, 30, frameCount);
-           
-       }
-       
-       
-       
-       return data;       
+        byte[] data = null;
+
+        if (!videoIsAvailable) {
+
+            dataLength = 1;
+            messageLength = dataLength + HEADER_LENGTH;
+            data = new byte[messageLength];
+
+            //Put Message Length
+            FlareMessage.intToData(data, 0, messageLength);
+            data[4] = flareOpCode;
+
+            //Followed by (byte) 0
+            data[5] = 0;
+            //System.out.println(data[5]);
+
+        } else {
+
+            dataLength = 29;
+            messageLength = dataLength + HEADER_LENGTH;
+            data = new byte[messageLength];
+
+            FlareMessage.intToData(data, 0, messageLength);
+            data[4] = flareOpCode;
+
+            //Followed by (byte) 0
+            data[5] = 1;
+
+            //width
+            FlareMessage.intToData(data, 6, width);
+
+            //height
+            FlareMessage.intToData(data, 10, height);
+
+            //fps
+            FlareMessage.doubleToData(data, 14, fps);
+
+            //duration
+            FlareMessage.doubleToData(data, 22, duration);
+
+            FlareMessage.intToData(data, 30, frameCount);
+
+        }
+
+        return data;
     }
-    
+
 }
